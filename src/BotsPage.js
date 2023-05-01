e132a208640baf50177deb4bec879d47641e7dad
jelizasilatei <silateijc90@gmail.com>
2023-05-01 15:09:09 +0300

    Fetch data from db.json API

--- src/BotsPage.js
+++ src/BotsPage.js
@@ -2,9 +2,20 @@ import React, {useEffect, useState} from "react";
 
 function BotsPage(){
     // Step one code
-    // const [bots,setBots] = useState([])
+    const [bots,setBots] = useState([])
 
     // Fetch data
+    useEffect(() => {
+        fetch("http://localhost:3000/bots")
+        .then(res => res.json())
+        .then(data => setBots(data))
+    },[])
+
+    return (
+        <div>
+            
+        </div>
+    )
     
 }
 
