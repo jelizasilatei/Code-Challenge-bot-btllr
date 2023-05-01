5f7c81214894d1716686115596b2e7579692916d
jelizasilatei <silateijc90@gmail.com>
2023-05-01 15:05:11 +0300

    App Component and BotsPage Component

---
+++ src/BotsPage.js
@@ -0,0 +1,11 @@
+import React, {useEffect, useState} from "react";
+
+function BotsPage(){
+    // Step one code
+    // const [bots,setBots] = useState([])
+
+    // Fetch data
+    
+}
+
+export default BotsPage;
